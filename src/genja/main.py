539d4ef85b33faa83319d74cacd99e89217911af
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2023-01-21 20:59:10 -0500

    Add an index template

--- src/genja/main.py
+++ src/genja/main.py
@@ -9,12 +9,55 @@ from jinja2 import Environment, FileSystemLoader
 from importlib.metadata import version
 
 
-def parse_markdown(indir, outdir, md, template):
+def build_index(args, md, template):
+    """
+    Build the index.html page.
+    """
+
+    # Get input and output directories
+    input_dir = args.input
+    output_dir = args.output
+
+    # Store items for the index.html template
+    items = []
+
+    for path in Path(input_dir).glob('**/*.md'):
+
+        with path.open() as f:
+            mdtext = f.read()
+
+        _ = md.convert(mdtext)
+        meta = md.Meta
+
+        parts = list(path.parts)
+
+        if len(parts) > 2:
+            section = parts[1]
+            link = f'/{parts[1]}/{parts[2].replace("md", "html")}'
+            title = meta['title'][0]
+            items.append({'section': section, 'link': link, 'title': title})
+
+    # Write index.html to output directory
+    index_html = template.render(items=items)
+    output_path = Path(f'{output_dir}/index.html')
+
+    with output_path.open('w') as f:
+        f.write(index_html)
+
+    md.reset()
+
+
+def build_pages(args, md, template):
     """
     Parse content of Markdown files and write to HTML files. If needed,
     subfolders are created too.
     """
-    for mdfile in Path(indir).glob('**/*.md'):
+
+    # Get input and output directories
+    input_dir = args.input
+    output_dir = args.output
+
+    for mdfile in Path(input_dir).glob('**/*.md'):
 
         with mdfile.open() as f:
             mdtext = f.read()
@@ -24,7 +67,7 @@ def parse_markdown(indir, outdir, md, template):
         page = template.render(data=meta, content=html)
 
         parts = list(mdfile.parts)
-        parts[0] = outdir
+        parts[0] = output_dir
         pathout = Path(*parts).with_suffix('.html')
         pathout.parent.mkdir(parents=True, exist_ok=True)
 
@@ -38,6 +81,7 @@ def main():
     """
     Main driver to run the program.
     """
+
     parser = argparse.ArgumentParser(description='Generate HTML files from Markdown files.')
     parser.add_argument('input', help='directory of Markdown files')
     parser.add_argument('output', help='directory for generated HTML files')
@@ -52,8 +96,10 @@ def main():
     md = markdown.Markdown(extensions=['meta', 'fenced_code'])
 
     env = Environment(loader=FileSystemLoader('templates'))
+    index_template = env.get_template('index.html')
     page_template = env.get_template('page.html')
 
-    parse_markdown(args.input, args.output, md, page_template)
+    build_index(args, md, index_template)
+    build_pages(args, md, page_template)
 
     print('DONE')
